05080d58ff82f0cbb5d3764f8d24faee3b7f2fba
kasunmadhumal <73273550+kasunmadhumal@users.noreply.github.com>
2024-01-30 18:14:33 +0530

    add garage service source codes (#3)

---
+++ garage-service/src/main/java/com/isa/garageservice/GarageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.isa.garageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageServiceApplication.class, args);
+	}
+
+}
