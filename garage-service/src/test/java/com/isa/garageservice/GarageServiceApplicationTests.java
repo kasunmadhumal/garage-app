c12eee49707a6ae5d93676109cf49faf09219e04
kasunmadhumal <kasunmadhumal98@gmail.com>
2024-01-30 18:11:25 +0530

    add garage service source codes

---
+++ garage-service/src/test/java/com/isa/garageservice/GarageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.garageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
