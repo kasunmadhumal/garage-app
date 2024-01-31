11fcefce27d6c8840e10d732c2fd0b678ec6c718
kasunmadhumal <kasunmadhumal98@gmail.com>
2024-01-31 15:23:37 +0530

    remove garage service

--- garage-service/src/main/java/com/isa/garageservice/authService/token/TokenType.java
+++
@@ -1,5 +0,0 @@
-package com.isa.garageservice.authService.token;
-
-public enum TokenType {
-    BEARER
-}
--- garage-service/src/main/java/com/isa/garageservice/dbConfig/ScopeNames.java
+++
@@ -1,5 +0,0 @@
-package com.isa.garageservice.dbConfig;
-
-public class ScopeNames {
-    public static final String DEFAULT_SCOPE = "_default";
-}
